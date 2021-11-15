e21eb4829f57818d43ee4bb420702beaf737e4cc
moween <moween1998@gmail.com>
2021-11-15 03:29:32 +0100

    Solve challenge

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,27 +1,131 @@
-function seed() {}
+function seed(...args) {
+  return args;
+}
 
-function same([x, y], [j, k]) {}
+function same([x, y], [j, k]) {
+  return x === j && y === k;
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  return !!this.find(livingCell => same(cell, livingCell));
+}
 
-const printCell = (cell, state) => {};
+const printCell = (cell, state) => {
+  if (contains.call(state, cell)) {
+    return '\u25A3';
+  }
 
-const corners = (state = []) => {};
+  return '\u25A2';
+};
 
-const printCells = (state) => {};
+const minMax = (values = []) => {
+  return values.reduce((acc, value) => {
+    if (value > acc.max) {
+      acc.max = value;
+    }
+    if (value < acc.min) {
+      acc.min = value;
+    }
 
-const getNeighborsOf = ([x, y]) => {};
+    return acc;
+  }, { min: values[0] || 0, max: values[0] || 0 });
+}
 
-const getLivingNeighbors = (cell, state) => {};
+const corners = (state = []) => {
+  const [allX, allY] = state.reduce((acc, [x, y]) => {
+    acc[0].push(x);
+    acc[1].push(y);
 
-const willBeAlive = (cell, state) => {};
+    return acc;
+  }, [[], []]);
 
-const calculateNext = (state) => {};
+  const { min: minX, max: maxX } = minMax(allX);
+  const { min: minY, max: maxY } = minMax(allY);
 
-const iterate = (state, iterations) => {};
+  return {
+    topRight: [maxX, maxY],
+    bottomLeft: [minX, minY],
+  }
+};
 
-const main = (pattern, iterations) => {};
+const printCells = (state) => {
+  const { topRight, bottomLeft } = corners(state);
+  let board = '';
+  
+  for (let y = topRight[1]; y >= bottomLeft[1]; y -= 1) {
+    for (let x = bottomLeft[0]; x <= topRight[0]; x += 1) {
+      board += printCell([x, y], state) + ' ';
+    }
+
+    board = board.trim() + '\n';
+  }
+
+  return board;
+};
+
+const gridBoundedByCorners = (bottomLeft = [], topRight = []) => {
+  const grid = [];
+
+  for (let x = bottomLeft[0]; x <= topRight[0]; x += 1) {
+    for (let y = bottomLeft[1]; y <= topRight[1]; y += 1) {
+      grid.push([x, y]);
+    }
+  }
+
+  return grid;
+}
+
+const getNeighborsOf = ([x, y]) =>  gridBoundedByCorners([x - 1, y - 1], [x + 1, y + 1]).filter(cell => !same([x, y], cell));
+
+const getLivingNeighbors = (cell, state) => {
+  const neighbors = getNeighborsOf(cell);
+
+  return neighbors.filter(neighbor => contains.bind(state)(neighbor));
+};
+
+const willBeAlive = (cell, state) => {
+  const livingNeighbors = getLivingNeighbors(cell, state);
+
+  return (
+    livingNeighbors.length === 3 ||
+    (livingNeighbors.length === 2 && contains.bind(state)(cell))
+  );
+};
+
+const calculateNext = (state) => {
+  const { topRight, bottomLeft } = corners(state);
+  const newBottomLeft = [bottomLeft[0] - 1, bottomLeft[1] - 1];
+  const newTopRight = [topRight[0] + 1, topRight[1] + 1];
+  const newGrid = gridBoundedByCorners(newBottomLeft, newTopRight);
+
+  return newGrid.filter(cell => willBeAlive(cell, state));
+};
+
+const iterate = (state, iterations) => {
+  const gameStates = [state];
+
+  let intermediateState = [...state];
+  for (let times = 0; times < iterations; times += 1) {
+    intermediateState = calculateNext(intermediateState);
+    gameStates.push([...intermediateState]);
+  }
+
+  return gameStates;
+};
+
+const main = (pattern, iterations) => {
+  let initialState = [...startPatterns[pattern]];
+  if (!initialState) {
+    console.log('');
+
+    return;
+  };
+
+  const states = iterate(initialState, iterations);
+
+  console.log(states.map(state => printCells(state)).join('\n'));
+};
 
 const startPatterns = {
     rpentomino: [
